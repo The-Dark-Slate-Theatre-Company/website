0144603b87b3a78bf774c726e0753a8c7c3dc934
Thomas McCarthy <thomasjmmccarthy@gmail.com>
2026-06-26 21:15:33 +0100

    Added address book padding

--- src/pages/admin/address-book/AddressBook.jsx
+++ src/pages/admin/address-book/AddressBook.jsx
@@ -63,7 +63,7 @@ export function AddressBook() {
       {
         filteredContacts && 
         <> 
-          <div className='w-full flex justify-center'>
+          <div className='w-full flex justify-center pb-12'>
             <div className='w-full max-w-250 grid grid-cols-[30fr_1fr] gap-6 mb-4'>
               <div className='w-full bg-black px-4 py-2 border-white/20 border rounded-sm flex gap-4 items-center'>
                 <Search className='text-(--accent)' size={20} />
